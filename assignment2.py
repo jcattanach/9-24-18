a5ecd6da798e75b42881550089d21082145d8cc4
Jack Cattanach <jackcattanach@live.com>
2018-09-24 19:33:34 -0500

    Assignments 1 and 2 have been completed in this second commit

--- assignment2.py
+++ assignment2.py
@@ -1,31 +1,21 @@
-#Still need to finish remove ability and user id incrementing
-#
-
-
-
-
-
-# TO DO LIST ASSIGNMENT
 class Task:
     def __init__(self, title, priority):
         self.title = title
         self.priority = priority
-        self.id = 1
-
-    def increment_id(self):
-        self.id = self.id + 1
 
     def __repr__(self):
-        return "** TO-DO ITEM {0}: {1}\n PRIORITY: {2} **\n".format(self.id, self.title, self.priority)
+        return "** TO-DO ITEM: {0}\n PRIORITY: {1} **\n".format(self.title, self.priority)
 
 tasks = []
 
 while True:
 
+
     print("**** WELCOME TO TO-DO LIST ****\n(press 'q' to quit at anytime)")
 
     choice = input("What would you like to do?\na-add new task, \nq-quit, \nr-remove, \nv-view list: ").upper()
     if choice == 'A':
+
         task_input = input("Enter your task: ").upper()
         if task_input == 'Q':
             break
@@ -35,15 +25,16 @@ while True:
 
         task1 = Task(task_input, priority_input)
 
-        task1.increment_id()
-
         tasks.append(task1)
 
 
-    # if choice == 'R':
-    #     del tasks[:]
-    #     print("Items removed")
-    #     print(tasks)
+    if choice == 'R':
+        remove_option = input("Do you want to remove tasks? (y/n): ").upper()
+        if remove_option == 'Y':
+            tasks = []
+            print("List cleared.")
+        elif choice == 'Q':
+            break
 
     if choice == 'V':
         print(tasks)
